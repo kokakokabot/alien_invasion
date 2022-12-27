b877f832fd22089ac05fa7353026d475df414fba
Damir Kokic <kokicd1@gmail.com>
2022-12-27 19:27:41 +0100

    First commit for Space Invaders Game

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings():
+    """A class to store all setting for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        #Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+
+        #Ship settings
+        self.ship_speed_factor = 1.5
